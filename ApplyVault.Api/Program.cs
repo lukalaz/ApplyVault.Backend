b76e26b3721ba0f53d3520b4bd92bc688ee97fdb
Luka Lazic <luka.web.php@gmail.com>
2026-02-14 14:28:54 +0100

    Add swagger

--- ApplyVault.Api/Program.cs
+++ ApplyVault.Api/Program.cs
@@ -5,8 +5,24 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApplyVault API v1");
+        c.RoutePrefix = "swagger";
+    });
+}
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
